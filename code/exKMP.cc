f0ddc4f7f83b11b92ff4ccee7df34a926f5643d8
Simon-Lon <34621564+Simon-Lon@users.noreply.github.com>
2019-10-25 21:59:54 +0800

    update:exkmp

--- code/exKMP.cc
+++ code/exKMP.cc
@@ -1,37 +1,43 @@
-const int maxn = "Edit";
-int ans, nexr[maxn], ex[maxn];
-void getnexr(char s[]) {
-	int i = 0, j, po, len = strlen(s);
-	nexr[0] = len;
-	while (s[i] == s[i+1] && i + 1 < len) i++;
-	nexr[1] = i;
-	po = 1;
-	for (i = 2; i < len; i++) {
-		if (nexr[i-po] + i < nexr[po] + po) {
-			nexr[i] = nexr[i - po];
-		} else {
-		 	j = nexr[po] + po - i;
-		 	if (j < 0) j = 0;
-		 	while (i + j < len && s[j] == s[i+j]) j++;
-		 	nexr[i] = j;
-		 	po = i;
-		 }
-	}
-}
+/*
+ * Author: nuchenghao
+ * 复杂度: O(n+m)
+ * 功能: 解决如下问题：定义母串s和子串p，设s的长度为n,p的长度为m,求p与s的每一个后缀的最长公共前缀
+ *      extend[i]表示T与s[i,n-1]的最长公共前缀。（0<=i<n）
+ *      一个辅助工具为nxt[i]表示p[i,m-1]和T的最长公共前缀长度
+ *      下标都从0开始
+ */
 
-void exkmp(char s1[], char s2[]) {
-	int i = 0, j, po = 0, len = strlen(s1), l2 = strlen(s2);
-	while (s1[i] == s2[i] && i < l2 && i < len) i++;
-	ex[0] = i;
-	for (i = 1; i < len; i++) {
-		if (nexr[i - po] + i < ex[po] + po) {
-			ex[i] = nexr[i-po];
-		} else {
-		 	j = ex[po] + po - i;
-		 	if (j < 0) j = 0;
-		 	while (i + 1 < len && s1[j+i] == s2[j]) j++;
-		 	ex[i] = j;
-		 	po = i;
-		 }
-	}
+int nxt[maxn], ex[maxn]; /*ex数组即为extend数组 */
+/*预处理计算next数组 */
+void getnxt(char *s) {
+    int i = 0, j, po, len = strlen(s);
+    nxt[0] = len;   /*初始化next[0] */
+    while (s[i] == s[i + 1] && i + 1 < len) i++; /*计算next[1] */
+    nxt[1] = i; po = 1; /*初始化po的位置 */
+    for (i = 2; i < len; i++) {
+        if (nxt[i - po] + i < nxt[po] + po) nxt[i] = nxt[i - po];/*第一种情况，可以直接得到next[i]的值 */
+        else { /*第二种情况，要继续匹配才能得到next[i]的值 */
+            j = nxt[po] + po - i;
+            if (j < 0) j = 0; /*如果i>po+nxt[po],则要从头开始匹配 */
+            while (i + j < len && s[j] == s[j + i]) j++; /*计算next[i] */
+            nxt[i] = j;
+            po = i; /*更新po的位置 */
+        }
+    }
+}
+/*计算extend数组 */
+void exkmp(char *s, char *p) {
+    int i = 0, j, po, len = strlen(s), l2 = strlen(p);
+    getnxt(p); /*计算子串的next数组 */
+    while (s[i] == p[i] && i < l2 && i < len) i++;/*计算ex[0] */
+    ex[0] = i; po = 0; //初始化po的位置
+    for (i = 1; i < len; i++) {
+        if (nxt[i - po] + i < ex[po] + po) ex[i] = nxt[i - po] /*第一种情况，直接可以得到ex[i]的值 */
+        else {  /*第二种情况，要继续匹配才能得到ex[i]的值 */
+            j = ex[po] + po - i;
+            if (j < 0) j = 0; /*如果i>ex[po]+po则要从头开始匹配 */
+            while (i + j < len && j < l2 && s[j + i] == p[j]) j++;  /*计算ex[i] */
+            ex[i] = j; po = i; //更新po的位置
+        }
+    }
 }
